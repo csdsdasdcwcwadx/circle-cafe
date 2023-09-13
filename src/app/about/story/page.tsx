7e1597fe79c6557a84ff3dfc5f904e27206a31b2
蔡濡安 <jack0978330@gmail.com>
2023-09-13 22:48:32 +0800

    新增story頁面

--- src/app/about/story/page.tsx
+++ src/app/about/story/page.tsx
@@ -1,22 +1,37 @@
 'use client';
 
-import { Fragment, useEffect } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import { setPage } from '@/redux/actions';
 import { E_Page } from '@/redux/interfaces';
 import { useDispatch } from 'react-redux';
 import Footer from '@/components/Footer';
+import steakSrc from '../../../image/20221001122155672158.jpg';
+import Image from 'next/image';
+import cN from 'classnames';
 
 export default function Story() {
     const dispatch = useDispatch();
+    const [activate, setActivate] = useState(false);
 
     useEffect(() => {
         dispatch(setPage(E_Page.STORY));
+        setActivate(true);
     }, [dispatch])
 
     return (
         <Fragment>
-            <div className={styles.Story}>this is the story page</div>
+            <div className={cN(styles.Story, {[styles.active]: activate})}>
+                <div className={styles.mainbanner}>
+                    <Image src={steakSrc} alt='steak'/>
+                </div>
+                <main>
+                    <h2>品牌故事</h2>
+                    <p>
+                        123
+                    </p>
+                </main>
+            </div>
             <Footer/>
         </Fragment>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 // import { Inter } from 'next/font/google'
 import { Providers } from '@/redux/provider'
 import Header from '@/components/Header'
-import Footer from '@/components/Footer'
 import '../global.css';
 
 // const inter = Inter({ subsets: ['latin'] })
