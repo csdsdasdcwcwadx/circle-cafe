9cff0e6fa949482dca6f5e7c452bd8faf9598348
蔡濡安 <jack0978330@gmail.com>
2023-09-27 22:11:15 +0800

    新增廚師話頁面

--- src/app/about/message/page.tsx
+++ src/app/about/message/page.tsx
@@ -4,6 +4,8 @@ import Default from "@/components/Default";
 import styles from './styles.module.scss';
 import { E_Page } from "@/redux/interfaces";
 import messageSrc from '@/image/mcdongal.jpeg';
+import iconSrc from '@/image/FesOdoNUoAEaCAR.jpg';
+import Image from "next/image";
 
 export default function message() {
     return (
@@ -14,7 +16,14 @@ export default function message() {
             altContent="message"
             title="廚師對話"
         >
-            123
+            <div className={styles.section}>
+                <Image src={iconSrc} alt="icon"/>
+                <span>
+                    故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事事故事故事故事故事故事故事故事故
+                    事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故
+                    事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故事故
+                </span>
+                </div>
         </Default>
     )
 }
